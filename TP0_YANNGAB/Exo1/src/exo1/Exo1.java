c83ce6434551468f4a795d5b3020e4bfa1a14c59
yann <yann@10.3.60.218>
2024-09-23 16:15:49 +0200

    Création d'un nouveau projet versionné

---
+++ TP0_YANNGAB/Exo1/src/exo1/Exo1.java
@@ -0,0 +1,29 @@
+/*
+ TP0 Exercice 1:
+YannGAB
+Septembre 2024
+ */
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author yann
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonjour");
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Quel est votre prenom ?");
+        prenom = sc.nextLine();
+        System.out.println("Au revoir");
+    }
+    
+}
